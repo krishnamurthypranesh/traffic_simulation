7e0a314d4134581415667ce6d7ccbfb39ebe706c
Mik <mik23@protonmail.ch>
2018-03-25 20:23:30 +0530

    Changes to be committed: 	modified:   main.py 	 -> Changed the model to fix overtaking errors 	TODO: 	 -> Parse the array from rhs to lhs

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
+""" A traffic sumulation model based on the Nagel–Schreckenberg cellular automation where """
+
 import random
 
 class Vehical:
     def __init__(self, speed, id):
-        self.original = speed
         self.speed = speed
-        self.stopd = False
         self.id = id
 
-    def set_stop(self):
-        self.speed = 0
-        self.stopd = True
-
-    def reset(self):
-        self.speed = self.original
+    def set_speed(self, speed):
+        self.speed = speed
 
     def get_speed(self):
         return self.speed
@@ -23,48 +19,55 @@ class Model:
         self.length = length
         self.vehical = None
         # create an empty road of given length
-        self.road = [' ']*self.length
+        self.road = [" "]*self.length
 
     def generate_road(self, vehical):
         """ Generates a road with vehicals with a minimum of 1 cell space """
         self.vehical = vehical
-        if self.road[:1] == [' ']:
+        if self.road[:1] == [" "]:
             self.road[0] = vehical.id
         #print(self.road)
 
     def set_position(self):
-        """ Edits the position of the vehical based on the speed and position of the vehical """
+        """ Edits the position of the vehical based on the speed of the vehical """
         try:
             index = self.road.index(self.vehical.id)
-            if self.road[index+1] != ' ':
-                self.vehical.set_stop()
-                return
-            elif self.vehical.stopd:
-                self.vehical.reset()
+            distance = self.vehical.speed
+            if self.road[index+1] != " ":
+                """ If the cell ahed of the vehical is not empty"""
+                print('here 1')
+                self.vehical.set_speed(self.vehical.speed-1)
+            # elif self.road[index+1] != " ":
+            #     """ if the neighbouring cell is not empty """
+            if self.road[index+distance+1] != " ":
+                """ if the cell the vehical will appear next is not empty """
+                # self.vehical.set_speed(-1)
+                print('here 2')
+                print('id:', self.vehical.id, 'speed:', self.vehical.speed, 'distance:', distance, 'index:', index, end='')
+                self.vehical.set_speed(distance-index-1)
+                print(' new_speed',self.vehical.speed)
             distance = self.vehical.speed
             index += distance + 1
             self.road.remove(self.vehical.id)
             self.road.insert(index, self.vehical.id)
         except ValueError:
+            # for list inclusion
             pass
         except IndexError:
             # This exception occurs when the object is at the end of the list
-            self.road[index] = ' '
-
+            self.road[index] = " "
 
     def get_model(self):
         """ returns the road """
         return self.road
 
 # create 'n' random vehicals with random speeds (min->1, max->5)
-vehicals = [Vehical(random.randrange(1, 5), ''.join(random.sample("qwaszxedcrfvtgbyhnujkimlop", 5))) for _ in range(5)]
+vehicals = [Vehical(random.randrange(1, 5), ''.join(random.sample("qwaszxedcrfvtgbyhnujkimlop", 5))) for _ in range(10)]
 
-# create a road of length 1000
+# create a road of length 'l'
 road = Model(20)
 
-# iterate for a 1000 cycles
-for _ in range(10):
-    for vehical in vehicals:
-        road.generate_road(vehical)
-        print(road.get_model())
-        road.set_position()
+for i, vehical in enumerate(vehicals):
+    road.generate_road(vehical)
+    print(i, '==>',road.get_model())
+    road.set_position()
